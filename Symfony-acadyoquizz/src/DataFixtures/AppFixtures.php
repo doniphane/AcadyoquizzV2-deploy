ec4981f3d3990769788f9e9bc23e32d0659288d7
doniphane <trulesdoniphane974@gmail.com>
2025-09-05 12:48:32 +0400

    Add code coverage report for Symfony-acadyoquizz project

--- Symfony-acadyoquizz/src/DataFixtures/AppFixtures.php
+++ Symfony-acadyoquizz/src/DataFixtures/AppFixtures.php
@@ -9,8 +9,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'un utilisateur de test
+        $user = new \App\Entity\Utilisateur();
+        $user->setEmail('test@example.com');
+        $user->setLastName('Test');
+        $user->setFirstName('User');
+        $user->setPassword('$2y$13$examplehashpassword');
+        $manager->persist($user);
+
+        // Création d'un questionnaire de test
+        $questionnaire = new \App\Entity\Questionnaire();
+        $questionnaire->setTitre('Quiz de test');
+        $questionnaire->setCreePar($user);
+        $questionnaire->setCodeAcces('TEST123');
+        $questionnaire->setEstActif(true);
+        $questionnaire->setScorePassage(70);
+        $questionnaire->setDateCreation(new \DateTimeImmutable());
+        $manager->persist($questionnaire);
 
         $manager->flush();
     }
